a628f81a76362a53c15df48345b092ca3ee75bc6
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-14 18:01:11 +0600

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { auth, db } from "@/app/firebase";
+import { auth, db } from "../firebase";
 import { 
   collection, 
   addDoc, 
