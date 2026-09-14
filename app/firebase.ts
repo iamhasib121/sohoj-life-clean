191e211470a575e5981773049f59dbe8c4db379b
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-14 12:29:29 +0600

    Create firebase.ts

---
+++ app/firebase.ts
@@ -0,0 +1,16 @@
+// firebase.ts
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCshJ69Plij5SCq9bIW1GkrXaXIOhMCYWg",
+  authDomain: "rannadress.firebaseapp.com",
+  projectId: "rannadress",
+  storageBucket: "rannadress.firebasestorage.app",
+  messagingSenderId: "963035839894",
+  appId: "1:963035839894:web:8a9a2b96fa27a9ef4431b7",
+  measurementId: "G-5CF16C3NLD"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+export const auth = getAuth(app);
