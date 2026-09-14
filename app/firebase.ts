a96b07401a37d05c066cc33efbf99aa0be427c2f
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-14 14:12:10 +0600

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCshJ69Plij5SCq9bIW1GkrXaXIOhMCYWg",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
+export const db = getFirestore(app);
