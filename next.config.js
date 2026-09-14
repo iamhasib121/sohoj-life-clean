0c672f0da8a0ab75ea7bd2c818794135b4dea8c3
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-14 13:17:07 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-// next.config.js (রুট ফোল্ডারে, না থাকলে নতুন বানান)
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.devtool = 'source-map'; // eval-ভিত্তিক সোর্সম্যাপ এড়িয়ে যাবে
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
