addb82646865270f24c9bc6ccccd5a8f9417ba68
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-14 11:35:22 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Sohoj Life Store</h1>
+      <p>Welcome to Sohoj Life! Our site is successfully running.</p>
+    </main>
+  )
+}
