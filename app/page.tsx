366c4afd6cde590f005e7e7126850c71307c9882
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-16 16:07:28 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Header from "@/components/Header";
 import FlashSaleTimer from "@/components/FlashSaleTimer";
 import ProductDetails from "@/components/ProductDetails";
 import FaqSection from "@/components/FaqSection";
-import CustomerReviews from "@/components/CustomerReviews"; // 👈 কাস্টমার রিভিউ ইম্পোর্ট করলাম
-//import SocialShare from "@/components/SocialShare";           // 👈 সোশ্যাল শেয়ার ইম্পোর্ট করলাম
+import CustomerReviews from "@/components/CustomerReviews"; 
+//import SocialShare from "@/components/SocialShare";           
 import Footer from "@/components/Footer";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 
