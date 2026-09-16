cf4ea67618b82d1bd34536ee5b95bd216ec4bdc9
Md.hasibul hasan <72484940+iamhasib121@users.noreply.github.com>
2026-09-16 12:31:00 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 // CartProvider ইমপোর্ট করুন
-import { CartProvider } from "../src/context/CartContext";
+import { useCart } from "../../src/context/CartContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
